9af124d94e632ccba3ab569f88c699f454f024a6
Higor Castilho <castilhohf@gmail.com>
2020-08-10 13:20:31 -0300

    adding dotenv file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,17 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import routes from './routes'
 import cors from 'cors'
 
+dotenv.config()
+
+const { PORT } = process.env
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => {
+app.listen(PORT, () => {
 	console.log('Running on port 3333')
 })
