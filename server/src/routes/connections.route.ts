d85a1fd29c6916c17fc810d84c1994549f8961d6
Higor Castilho <castilhohf@gmail.com>
2020-08-11 16:28:11 -0300

    feat: auth build - now jwt verification on header or json is working

--- server/src/routes/connections.route.ts
+++ server/src/routes/connections.route.ts
@@ -2,9 +2,11 @@ import express from 'express'
 import ConnectionsController from '../controllers/ConnectionsController'
 const connectionsController = new ConnectionsController()
 
+import verifyJwt from '../utils/verifyJwt'
+
 const routes = express.Router()
 
-routes.get('/connections', connectionsController.index)
+routes.get('/connections', verifyJwt, connectionsController.index)
 routes.post('/connections', connectionsController.create)
 
 export default routes
---
+++ server/src/utils/verifyJwt.ts
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+import express from 'express'
+
+const routes = express.Router()
+
+export default routes.use(function(req, res, next) {
+
+	const token = req.body.token || req.query.token || req.headers['x-access-token']
+
+	if (token) {
+		jwt.verify(token, 'asasdasdsadf', function(err, decoded) {
+			if(err) {
+				return res.json({ message: 'Failed to validate token' })
+			} else {
+				req.decoded = decoded
+				next()
+			}
+		})
+	} else {
+		return res.status(403).send({
+			message: 'Failed to receive a token'
+		})
+	}
+})
